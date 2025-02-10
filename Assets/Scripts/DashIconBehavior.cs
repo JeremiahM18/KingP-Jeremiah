dbc1b2f1e9280952bc432c407f787d82b6904f34
Jeremiah <jermcdon18@gmail.com>
2025-02-10 15:53:59 -0600

    Added new scene, game button, dash icon
    Added a new scene and added a main game button and updated dash icon

---
+++ Assets/Scripts/DashIconBehavior.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+
+public class DashIconBehavior : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public float fill;
+    Image overlay;
+    float coolDownRate;
+    float coolDown;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        label = GetComponentInChildren<TextMeshProUGUI>();
+        Image[] images = GetComponentsInChildren<Image>();
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].tag == "overlay")
+            {
+                overlay = images[i];
+            }
+        }
+        //coolDownRate = PinBehavior.cooldownRate;
+        overlay.fillAmount = 0.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Cooldown = PinBehavior.cooldown;
+            string message = "";
+            if (coolDown > 0.0)
+            {
+                float fill = coolDown / coolDownRate;
+                message = string.Format("{0:0.0}", coolDown);
+                overlay.fillAmount = fill;
+            }
+            label.SetText(message);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void gotoGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+    }
+}
